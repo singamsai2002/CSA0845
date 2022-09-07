861deab45473320aecd663dd3260f986b93fa50f
singamsai2002 <112846532+singamsai2002@users.noreply.github.com>
2022-09-07 12:31:33 +0530

    Add files via upload

---
+++ person is eligible to vote.py
@@ -0,0 +1,8 @@
+n=int(input("Enter the age of person:"))
+if(n>=18):
+      print("The person is eligible to vote:")
+else:
+      a=18-age
+      print(a)
+      print("age left to eligible to vote:")
+         
